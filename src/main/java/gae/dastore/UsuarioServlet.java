17ecf585914c3ca8ac7a268e468b70a8592344eb
Antonio Marin <antonio.marin@ceiba.com.co>
2020-11-19 13:59:45 -0500

    GRAL: ESTABILIZACION

--- src/main/java/gae/dastore/UsuarioServlet.java
+++ src/main/java/gae/dastore/UsuarioServlet.java
@@ -1,30 +1,55 @@
 package gae.dastore;
 
 import java.io.IOException;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import gae.dastore.persist.User;
 import gae.dastore.persist.UsuarioDAO;
 
+@Path("/usuario")
+public class UsuarioServlet {
 
-@WebServlet(name = "UsuarioServlet", value = "/usuario")
-public class UsuarioServlet extends HttpServlet {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	@Context
+	private HttpServletRequest request;
+	@Context
+	private HttpServletResponse response;
 
-	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON+ ";charset=utf-8")
+	public Response doPost(User user) throws IOException {
 
-		UsuarioDAO usuario = new UsuarioDAO();		
-		usuario.guardar(request.getParameter("nombre"), request.getParameter("apellido"));
+		UsuarioDAO usuario = new UsuarioDAO();
 		
-		
-		response.setContentType("text/html");
-		response.getWriter().println("<h1>user created!</h1>");
+		usuario.guardar(user);
+		return Response.ok("200",MediaType.APPLICATION_JSON).build();
 	}
+
+	
+//	public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//		UsuarioDAO usuario = new UsuarioDAO();
+//
+//		usuario.eliminar(request.getParameter("id"));
+//		response.setContentType("text/html");
+//		response.getWriter().println("<h1>user deleted!</h1>");
+//	}
+//
+//	@Override
+//	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//		UsuarioDAO usuario = new UsuarioDAO();
+//
+//		response.setContentType("application/json");
+//		PrintWriter out = response.getWriter();
+//		out.print(usuario.obtenerUsuarios().toString());
+//		out.flush();
+//
+//	}
 }
--- src/main/java/gae/dastore/persist/UsuarioDAO.java
+++ src/main/java/gae/dastore/persist/UsuarioDAO.java
@@ -1,29 +1,117 @@
 package gae.dastore.persist;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
-import com.google.cloud.datastore.FullEntity;
 import com.google.cloud.datastore.Key;
-import com.google.cloud.datastore.KeyFactory;
+import com.google.cloud.datastore.Value;
 
 public class UsuarioDAO {
 
-	Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+	public String guardar(User usuario) {
+		Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+		datastore.put(objectToEntity(usuario));
+		return usuario.getNombre();
 
+	}
 
-	public Entity guardar(String nombre, String apellido) {
-		
-		Datastore datastore = DatastoreOptions.getDefaultInstance().getService();		
-		KeyFactory usuarioKey = datastore.newKeyFactory().setKind("user");
-		
-		FullEntity entidad = Entity.newBuilder(usuarioKey.newKey())
-				.set("nombre", nombre)
-				.set("apellido", apellido)
-				.build();
-		
-		return datastore.put(entidad);
-		
+	public void eliminar(String id) {
+		Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+		Key usuarioKey = asignarKey(id);
+
+		datastore.delete(usuarioKey);
+	}
+
+//	public List<User> obtenerUsuarios() {
+//		List<User> usuarios = new ArrayList<User>();
+//		Query<Entity> query = Query.newEntityQueryBuilder().setKind("user").build();
+//		QueryResults<Entity> results = datastore.run(query);
+//		while (results.hasNext()) {
+//			User usuario = new User();
+//			Entity entity = results.next();
+//			usuario.setNombre(entity.getString("nombre"));
+//			usuario.setApellido(entity.getString("apellido"));
+//			usuario.setId(entity.getString("id"));
+//			usuarios.add(usuario);
+//		}
+//		return usuarios;
+//	}
+
+	private Key asignarKey(String value) {
+		Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+		Key key = datastore.newKeyFactory().setKind("user").newKey(value);
+
+		return key;
+	}
+
+	public Entity objectToEntity(Object object) {
+		Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+		Entity entity = null;
+		try {
+			String entityName = object.getClass().getSimpleName();
+			Field[] fields = object.getClass().getDeclaredFields();
+			for (Field f : fields) {
+				f.setAccessible(true);
+				String fieldType = f.getType().getSimpleName();
+				if (fieldType.equals("Key")) {
+					Key key = datastore.newKeyFactory().setKind(entityName).newKey((String) f.get(object));
+					entity = Entity.newBuilder(key).build();
+					break;
+				}
+			}
+
+			if (entity == null) {
+				// Si el id no es de tipo Key, lo buscamos
+				Object keyVal = null;
+				outerloop: for (Field f : fields) {
+					f.setAccessible(true);
+					Annotation[] anotaciones = f.getDeclaredAnnotations();
+					if (anotaciones == null)
+						continue;
+					for (Annotation annotation : anotaciones) {
+						String nombreAnotacion = annotation.annotationType().getSimpleName();
+						if (nombreAnotacion.equals("Id")) {
+							keyVal = f.get(object);
+							break outerloop;
+						}
+					}
+				}
+				if (keyVal != null) {
+					if (keyVal instanceof String) {
+						Key key = datastore.newKeyFactory().setKind(entityName).newKey((String) keyVal);
+						entity = Entity.newBuilder(key).build();
+					}
+//					} else if (keyVal instanceof Long) {
+//						Key key = datastore.newKeyFactory().setKind(entityName).newKey((Long) keyVal);
+//						entity = Entity.newBuilder(key).build();
+//					} else if (keyVal instanceof Integer) {
+//						Key key = datastore.newKeyFactory().setKind(entityName).newKey((Integer) keyVal);
+//						entity = Entity.newBuilder(key).build();
+//					}
+				} else {
+					throw new Exception("La entidad " + entityName + " no tiene Key");
+				}
+			}
+			for (Field f : fields) {
+				f.setAccessible(true);
+				String fieldName = f.getName();
+				String fieldType = f.getType().getSimpleName();
+				if (!fieldName.contains("jdo") && !fieldType.equals("Key")) {
+					Object fieldValue = f.get(object);
+					entity.newBuilder().set(fieldName, (Value<?>) fieldValue);
+				}
+			}
+			// setea clave primaria
+		} catch (
+
+		IllegalArgumentException ie) {
+			System.err.println(ie);
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+		return entity;
 	}
 
 }
